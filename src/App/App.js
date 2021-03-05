e4d52b7fa6a7ad6c76316cd00066897793799acc
mattdean <matthewallandean@gmail.com>
2021-03-05 11:15:31 -0700

    Set up another fetch

--- src/App/App.js
+++ src/App/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      rocketData: launchData
+      rocketData: launchData,
     }
   }
 
--- src/util.js
+++ src/util.js
@@ -1,13 +1,25 @@
-const getRocketData = () => {
+const getUpcomingRockets = () => {
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
   const requestOptions = {
   headers: myHeaders,
   };
 
-  return fetch("https://ll.thespacedevs.com/2.0.0/launch/", requestOptions)
+  return fetch("https://ll.thespacedevs.com/2.0.0/launch/upcoming", requestOptions)
     .then(response => response.json())
     .catch(error => console.log('error', error));
 }
 
-export default getRocketData;
+const getRecentRockets = () => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  const requestOptions = {
+  headers: myHeaders,
+  };
+
+  return fetch("https://ll.thespacedevs.com/2.0.0/launch/previous", requestOptions)
+    .then(response => response.json())
+    .catch(error => console.log('error', error));
+}
+
+export {getUpcomingRockets, getRecentRockets};
