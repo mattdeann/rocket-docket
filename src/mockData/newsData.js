76c62003f1d8a4d1927c9045df46b4e2281ac050
mattdean <matthewallandean@gmail.com>
2021-03-05 11:10:37 -0700

    Set up conditional rendering based on state

---
+++ src/mockData/newsData.js
@@ -0,0 +1,84 @@
+const newsData = [
+  {
+  "id": "60423bf135c4a0001c42b8a3",
+  "title": "Watch live: Spacewalk underway to continue assembly of solar array fixtures",
+  "url": "https://spaceflightnow.com/2021/03/05/us-eva-72/",
+  "imageUrl": "https://mk0spaceflightnoa02a.kinstacdn.com/wp-content/uploads/2021/03/50884176938_bfe8eab442_k.jpg",
+  "newsSite": "Spaceflight Now",
+  "summary": "NASA astronaut Kate Rubins and Soichi Noguchi, a Japanese astronaut who last walked in space more than 15 years ago, ventured outside the International Space Station Friday to continue solar power system upgrades and to finish wiring up a European experiment platform.",
+  "publishedAt": "2021-03-05T14:10:57.000Z",
+  "updatedAt": "2021-03-05T14:57:33.238Z",
+  "featured": false,
+  "launches": [],
+  "events": [
+  {
+  "id": "212",
+  "provider": "Launch Library 2"
+  }
+  ]
+  },
+  {
+  "id": "604232a135c4a0001c42b898",
+  "title": "Planet-hunting eye of PLATO",
+  "url": "https://sci.esa.int/web/plato/-/planet-hunting-eye-of-plato",
+  "imageUrl": "https://cdn.sci.esa.int/documents/33240/35301/PLATO_camera_STM_testing_600.jpg",
+  "newsSite": "ESA",
+  "summary": "Key technology for ESA's exoplanet-hunting PLATO spacecraft has passed a trial by vacuum to prove the mission will work as planned. This test replica of an 80-cm high, 12-cm aperture camera spent 17 days inside a thermal vacuum chamber.",
+  "publishedAt": "2021-03-05T13:31:13.000Z",
+  "updatedAt": "2021-03-05T13:31:13.294Z",
+  "featured": false,
+  "launches": [],
+  "events": []
+  },
+  {
+  "id": "604226ea35c4a0001c42b886",
+  "title": "Whitesides steps down from Virgin Galactic",
+  "url": "https://spacenews.com/whitesides-steps-down-from-virgin-galactic/",
+  "imageUrl": "https://spacenews.com/wp-content/uploads/2020/07/vg-ceos.jpg",
+  "newsSite": "SpaceNews",
+  "summary": "The longtime chief executive of Virgin Galactic, who moved into a new position at the company last year, has left the company but will continue to be an adviser to it.",
+  "publishedAt": "2021-03-05T12:41:14.000Z",
+  "updatedAt": "2021-03-05T12:41:14.755Z",
+  "featured": false,
+  "launches": [],
+  "events": []
+  },
+  {
+  "id": "60421fde35c4a0001c42b87d",
+  "title": "Station power upgrade preparations continue with EVA-72",
+  "url": "https://www.nasaspaceflight.com/2021/03/station-power-upgrade-spacewalk/",
+  "imageUrl": "https://www.nasaspaceflight.com/wp-content/uploads/2020/06/Spacewalk_ISS_NASA_5.19.15.jpg",
+  "newsSite": "NASA Spaceflight",
+  "summary": "Two astronauts aboard the International Space Station (ISS) are conducting a spacewalk to continue the process of increasing the Station’s electrical power generation output. ",
+  "publishedAt": "2021-03-05T12:02:00.000Z",
+  "updatedAt": "2021-03-05T14:02:33.697Z",
+  "featured": false,
+  "launches": [],
+  "events": [
+  {
+  "id": "212",
+  "provider": "Launch Library 2"
+  }
+  ]
+  },
+  {
+  "id": "60421fe635c4a0001c42b87f",
+  "title": "Rocket Report: SpaceX explains landing failure, more on New Glenn delays",
+  "url": "https://arstechnica.com/science/2021/03/rocket-report-starliner-launch-slips-nasa-vet-says-agency-should-rethink-sls/",
+  "imageUrl": "https://cdn.arstechnica.net/wp-content/uploads/2021/03/SN10-Mar-3-2021-0369.jpg",
+  "newsSite": "Arstechnica",
+  "summary": "\"The level of detail that we get is remarkable, how good the forecast is.\"",
+  "publishedAt": "2021-03-05T12:00:59.000Z",
+  "updatedAt": "2021-03-05T14:56:19.498Z",
+  "featured": false,
+  "launches": [
+  {
+  "id": "f1bcfbf6-d00d-42d7-bc68-82252817a57e",
+  "provider": "Launch Library 2"
+  }
+  ],
+  "events": []
+  }
+]
+
+export default newsData;
---
+++ src/mockData/recentRocketsData.js
@@ -0,0 +1,185 @@
+const recentRocketsData = {
+  "count": 1702,
+  "next": "https://ll.thespacedevs.com/2.0.0/launch/previous/?limit=10&offset=10",
+  "previous": null,
+  "results": [
+      {
+          "id": "dfd4f0e0-0ab4-494d-bd88-1b93b934b269",
+          "url": "https://ll.thespacedevs.com/2.2.0/launch/dfd4f0e0-0ab4-494d-bd88-1b93b934b269/",
+          "launch_library_id": null,
+          "slug": "falcon-9-block-5-starlink-17",
+          "name": "Falcon 9 Block 5 | Starlink 17",
+          "status": {
+              "id": 3,
+              "name": "Success"
+          },
+          "net": "2021-03-04T08:24:00Z",
+          "window_end": "2021-03-04T08:24:00Z",
+          "window_start": "2021-03-04T08:24:00Z",
+          "inhold": false,
+          "tbdtime": false,
+          "tbddate": false,
+          "probability": 90,
+          "holdreason": "",
+          "failreason": "",
+          "hashtag": null,
+          "launch_service_provider": {
+              "id": 121,
+              "url": "https://ll.thespacedevs.com/2.2.0/agencies/121/",
+              "name": "SpaceX",
+              "type": "Commercial"
+          },
+          "rocket": {
+              "id": 2820,
+              "configuration": {
+                  "id": 164,
+                  "launch_library_id": 188,
+                  "url": "https://ll.thespacedevs.com/2.2.0/config/launcher/164/",
+                  "name": "Falcon 9 Block 5",
+                  "family": "Falcon",
+                  "full_name": "Falcon 9 Block 5",
+                  "variant": "Block 5"
+              }
+          },
+          "mission": {
+              "id": 1229,
+              "launch_library_id": null,
+              "name": "Starlink 17",
+              "description": "A batch of 60 satellites for Starlink mega-constellation - SpaceX's project for space-based Internet communication system.",
+              "launch_designator": null,
+              "type": "Communications",
+              "orbit": {
+                  "id": 8,
+                  "name": "Low Earth Orbit",
+                  "abbrev": "LEO"
+              }
+          },
+          "pad": {
+              "id": 87,
+              "url": "https://ll.thespacedevs.com/2.2.0/pad/87/",
+              "agency_id": null,
+              "name": "Launch Complex 39A",
+              "info_url": null,
+              "wiki_url": "https://en.wikipedia.org/wiki/Kennedy_Space_Center_Launch_Complex_39#Launch_Pad_39A",
+              "map_url": "http://maps.google.com/maps?q=28.608+N,+80.604+W",
+              "latitude": "28.60822681",
+              "longitude": "-80.60428186",
+              "location": {
+                  "id": 27,
+                  "url": "https://ll.thespacedevs.com/2.2.0/location/27/",
+                  "name": "Kennedy Space Center, FL, USA",
+                  "country_code": "USA",
+                  "map_image": "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/launch_images/location_27_20200803142447.jpg",
+                  "total_launch_count": 184,
+                  "total_landing_count": 0
+              },
+              "map_image": "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/launch_images/pad_87_20200803143537.jpg",
+              "total_launch_count": 127
+          },
+          "webcast_live": true,
+          "image": "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/launch_images/falcon2520925_image_20210121062346.png",
+          "infographic": "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/launch_images/falcon2520925_infographic_20210304014808.jpeg",
+          "program": []
+      },
+      {
+          "id": "c0ac9a61-5aac-40d7-9919-3c29ea7d4172",
+          "url": "https://ll.thespacedevs.com/2.2.0/launch/c0ac9a61-5aac-40d7-9919-3c29ea7d4172/",
+          "launch_library_id": null,
+          "slug": "starship-sn10-10-km-flight",
+          "name": "Starship SN10 | 10 km Flight",
+          "status": {
+              "id": 3,
+              "name": "Success"
+          },
+          "net": "2021-03-03T23:15:00Z",
+          "window_end": "2021-03-04T00:00:00Z",
+          "window_start": "2021-03-03T15:00:00Z",
+          "inhold": false,
+          "tbdtime": false,
+          "tbddate": false,
+          "probability": null,
+          "holdreason": "",
+          "failreason": "",
+          "hashtag": null,
+          "launch_service_provider": {
+              "id": 121,
+              "url": "https://ll.thespacedevs.com/2.2.0/agencies/121/",
+              "name": "SpaceX",
+              "type": "Commercial"
+          },
+          "rocket": {
+              "id": 2832,
+              "configuration": {
+                  "id": 207,
+                  "launch_library_id": null,
+                  "url": "https://ll.thespacedevs.com/2.2.0/config/launcher/207/",
+                  "name": "Starship Prototype",
+                  "family": "Starship",
+                  "full_name": "Starship Prototype",
+                  "variant": "Prototype"
+              }
+          },
+          "mission": {
+              "id": 1239,
+              "launch_library_id": null,
+              "name": "10 km Flight",
+              "description": "The SN10 Starship performed a test flight similar to SN8 and SN9. It launched up to an altitude of 10 km or 33,000 ft, did a belly flop maneuver followed by a controlled descent to the landing pad. Despite successfully performing the landing flip maneuver with its three Raptor engines, its vertical velocity was too high on landing, damaging its structure and resulting in an explosive destruction a few minutes after touchdown.",
+              "launch_designator": null,
+              "type": "Test Flight",
+              "orbit": {
+                  "id": 15,
+                  "name": "Suborbital",
+                  "abbrev": "Sub"
+              }
+          },
+          "pad": {
+              "id": 111,
+              "url": "https://ll.thespacedevs.com/2.2.0/pad/111/",
+              "agency_id": null,
+              "name": "Launch Pad A",
+              "info_url": null,
+              "wiki_url": "https://en.wikipedia.org/wiki/SpaceX_South_Texas_Launch_Site",
+              "map_url": "https://www.google.com/maps?q=25.997116+N,+97.15503099856647+W",
+              "latitude": "25.997116",
+              "longitude": "-97.15503099856647",
+              "location": {
+                  "id": 143,
+                  "url": "https://ll.thespacedevs.com/2.2.0/location/143/",
+                  "name": "SpaceX Space Launch Facility, TX, USA",
+                  "country_code": "USA",
+                  "map_image": "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/launch_images/location_143_20200803142438.jpg",
+                  "total_launch_count": 7,
+                  "total_landing_count": 7
+              },
+              "map_image": "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/launch_images/pad_111_20200803143229.jpg",
+              "total_launch_count": 6
+          },
+          "webcast_live": true,
+          "image": "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/launch_images/starship2520sn_image_20210304160023.jpeg",
+          "infographic": null,
+          "program": [
+              {
+                  "id": 1,
+                  "url": "https://ll.thespacedevs.com/2.2.0/program/1/",
+                  "name": "SpaceX Starship",
+                  "description": "The SpaceX Starship is a fully reusable super heavy-lift launch vehicle under development by SpaceX since 2012, as a self-funded private spaceflight project.  The second stage of the Starship — is designed as a long-duration cargo and passenger-carrying spacecraft. It is expected to be initially used without any booster stage at all, as part of an extensive development program to prove out launch-and-landing and iterate on a variety of design details, particularly with respect to the vehicle's atmospheric reentry.",
+                  "agencies": [
+                      {
+                          "id": 121,
+                          "url": "https://ll.thespacedevs.com/2.2.0/agencies/121/",
+                          "name": "SpaceX",
+                          "type": "Commercial"
+                      }
+                  ],
+                  "image_url": "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/program_images/spacex2520star_program_20201129204513.png",
+                  "start_date": "2019-03-01T05:00:00Z",
+                  "end_date": null,
+                  "info_url": "https://www.spacex.com/vehicles/starship/",
+                  "wiki_url": "https://en.wikipedia.org/wiki/SpaceX_Starship"
+              }
+          ]
+      },
+  ]
+}
+
+export default recentRocketsData;
