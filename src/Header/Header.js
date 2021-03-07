a975a729084786329b5f281ada8d2dd1e5c0b036
mattdean <matthewallandean@gmail.com>
2021-03-06 21:00:53 -0700

    Fix responsiveness

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './Header.css';
 import SearchBar from '../SearchBar/SearchBar';
+import logo from '../assets/rocket-docket-logo.png';
 
 function Header({filterRockets}) {
 
   return (
     <header className="header">
       <section className="left-header">
-        <h1 className="site-title">
-          Rocket Docket
-        </h1>
-        <p className="tagline" >
-          A docket of upcoming rocket launches.
-        </p>
+        <img className="logo" src={logo} alt="logo" />
+        <article className="site-banner">
+          <h1 className="site-title">
+            Rocket Docket
+          </h1>
+          <p className="tagline" >
+            A docket of upcoming rocket launches.
+          </p>
+        </article>
       </section>
       <section className="right-header">
         <SearchBar filterRockets={filterRockets} />
