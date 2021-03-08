7d94f29f49116d1bf8f203a9fa8640e8945d1aab
mattdean <matthewallandean@gmail.com>
2021-03-08 16:30:54 -0700

    Add additional tests for error component

--- cypress/integration/RocketDetails_spec.js
+++ cypress/integration/RocketDetails_spec.js
@@ -17,7 +17,7 @@ describe('Rocket Details', () => {
   })
 
   it('should have details about a rocket', () => {
-    cy.visit('http://localhost:3000/rocket-docket')
+    cy.visit('http://localhost:3000/rocket-docket/')
     cy.get('.more-info').first().click()
 
     cy.url().should('eq', 'http://localhost:3000/rocket-docket/falcon-9-block-5-starlink-17')
@@ -27,7 +27,8 @@ describe('Rocket Details', () => {
       cy.get('.summary.info-header').should('have.text', 'Mission Summary:')
       cy.get('.summary.info-content').should('have.text', 'A batch of 60 satellites for Starlink mega-constellation - SpaceX\'s project for space-based Internet communication system.')
       cy.get('.date.info-header').should('have.text', 'Launch Date and Time:')
-      cy.get('.date.info-content').should('have.text', '2021-03-04T08:24:00Z')
+      cy.get('.start-date').should('have.text', '2021-03-04')
+      cy.get('.start-time').should('have.text', '08:24:00Z')
       cy.get('.agency.info-header').should('have.text', 'Launch Agency:')
       cy.get('.agency.info-content').should('have.text', 'SpaceX')
       cy.get('.location.info-header').should('have.text', 'Location:')
@@ -41,8 +42,14 @@ describe('Rocket Details', () => {
     cy
       .get('.rocket-image').should('have.attr', 'src', 'https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/launch_images/falcon2520925_image_20210121062346.png')
       .get('.back-button').should('have.text', 'Back to Home')
+  })
 
-
-
+  it('should show the error component if a bad path is entered, and allow the user to return to home', () => {
+    cy.visit('http://localhost:3000/rocket-docket/some-bad-path')
+    cy
+      .get('.error-logo').should('have.attr', 'src', '/static/media/rocket-docket-logo.741c2a33.png')
+      .get('.error-message').should('have.text', 'Oops! An error with message"404 Page not found"has occured.')
+      .get('.error-back-button').should('have.text', 'Back to Home').click()
+      cy.url().should('eq', 'http://localhost:3000/rocket-docket')
   })
 })
