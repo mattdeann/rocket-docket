67abed0876c8ce91569258af3bf2ffac193d4d7e
mattdean <matthewallandean@gmail.com>
2021-03-20 13:42:52 -0600

    Fix bug with missing info

--- src/RocketDetails/RocketDetails.js
+++ src/RocketDetails/RocketDetails.js
@@ -12,34 +12,34 @@ function RocketDetails({findRocket, id}) {
       <main className="rocket-details">
         <section className="rocket-info">
           <h2 className="rocket-name">
-            {rocket.name}
+            {rocket.name || "No infomation available"}
           </h2>
           <h3 className="summary info-header">
             Mission Summary:
           </h3>
           <p className="summary info-content" >
-            {rocket.mission.description}
+            {rocket.mission.description || "No infomation available"}
           </p>
           <h3 className="date info-header">
             Launch Date and Time:
           </h3>
           <p className="start-date">
-          {rocket.window_start.slice(0, 10)}
+          {rocket.window_start.slice(0, 10) || "No infomation available"}
           </p>
           <p className="start-time">
-            {rocket.window_start.slice(11)}
+            {rocket.window_start.slice(11) || "No infomation available"}
           </p>
           <h3 className="agency info-header">
             Launch Agency:
           </h3>
           <p className="agency info-content" >
-            {rocket.launch_service_provider.name}
+            {rocket.launch_service_provider.name || "No infomation available"}
           </p>
           <h3 className="location info-header">
             Location:
           </h3>
           <p className="location info-content" >
-            {rocket.pad.name} at {rocket.pad.location.name}
+            {rocket.pad.name || "No infomation available"} at {rocket.pad.location.name || "No infomation available"}
           </p>
           <h3 className="webcast info-header">
           {rocket.webcast_live ? "Webcast available elsewhere": "No webcast available"}
